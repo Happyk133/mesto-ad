d99f85d3fe1ea861ebdc2e7e111b5b6aca4a4bc9
Happyk133 <santaly7878@gmail.com>
2026-05-31 17:06:35 +0700

    1234

---
+++ src/scripts/components/api.js
@@ -0,0 +1,71 @@
+const config = {
+  baseUrl: "https://mesto.nomoreparties.co/v1/apf-cohort-203",
+  headers: {
+    authorization: "b4bd1047-a098-41b9-a04b-66f38cbc34d4",
+    "Content-Type": "application/json",
+  },
+};
+
+const getResponseData = (res) => {
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+}; 
+
+export const getUserInfo = () => {
+  return fetch(`${config.baseUrl}/users/me`, { // Запрос к API-серверу
+    headers: config.headers, // Подставляем заголовки
+  }).then(getResponseData);  // Проверяем успешность выполнения запроса
+};
+
+export const getCardList = () => {
+  return fetch(`${config.baseUrl}/cards`, {
+    headers: config.headers,
+  }).then(getResponseData);
+};
+
+export const setUserInfo = ({ name, about }) => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      name,
+      about,
+    }),
+  }).then(getResponseData);
+};
+
+export const setUserAvatar = (avatarURL) => {
+  return fetch(`${config.baseUrl}/users/me/avatar`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      avatar: avatarURL
+    }),
+  }).then(getResponseData);
+};
+
+export const createCard = ({name, link}) => {
+  return fetch(`${config.baseUrl}/cards`, {
+    method: "POST",
+    headers: config.headers,
+    body: JSON.stringify({
+      name,
+      link,
+    }),
+  }).then(getResponseData);
+};
+
+export const removeCard = (cardId) => {
+  return fetch(`${config.baseUrl}/cards/${cardId}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then(getResponseData);
+};
+
+export const changeLikeCardStatus = (cardID, isLiked) => {
+  return fetch(`${config.baseUrl}/cards/likes/${cardID}`, {
+    method: isLiked ? "DELETE" : "PUT",
+    headers: config.headers,
+  }).then((res) => getResponseData(res));
+};
+
+
